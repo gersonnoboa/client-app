d85e4a5cfcb81b784e2e3682e1a93daeadb757c5
Gerson Noboa <gerson.noboa@glia.com>
2021-05-04 12:33:48 +0300

    Integrated Package into app

--- ClientApp/ViewController.swift
+++ ClientApp/ViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import ClosedSourceFramework
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let closedSourceStruct = ClosedSourceStruct()
+        print(closedSourceStruct.string)
+        closedSourceStruct.makeRequest()
     }
-
-
 }
 
